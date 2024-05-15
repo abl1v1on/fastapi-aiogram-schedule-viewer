09b1d5cc5545f935c3705752234977665b451300
abl1v1on <abl1v1on@yandex.ru>
2024-05-15 23:51:39 +0500

    set up project

--- telegram/main.py
+++ telegram/main.py
@@ -1,2 +1,23 @@
-from aiogram import Bot, dispatcher
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
+from config import settings
+
+
+bot = Bot(token=settings.API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f'Hello, {message.from_user.username}')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
